71a5fcf604c6ec331771e46a4efadf2185e60191
MikeNieuweboer <nieuweboermike@gmail.com>
2026-01-29 07:02:14 +0100

    Ran overnight MCAF tests

--- mcaf.sh
+++ mcaf.sh
@@ -1,3 +1,3 @@
 # Commands for the calculation of the mcaf score for the "Perfect Circle".
-uv run src/mcaf.py ./weights/GOLDEN_WEIGHTS.pt 50 1000 random;
-uv run src/mcaf.py ./weights/GOLDEN_WEIGHTS.pt 50 1000 blob
+uv run src/mcaf.py ./weights/Gr50-Ch8-Hi64_star.pt ./data/targets/star.png 100 4000 blob
+uv run src/mcaf.py ./weights/Gr50-Ch8-Hi64_star.pt ./data/targets/star.png 100 4000 random
